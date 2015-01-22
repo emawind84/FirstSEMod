248581404df1f26ddf15bc5713472dc7f6badaf8
Emanuele Disco <emawind84@gmail.com>
2015-01-22 14:30:49 +0900

    update

---
+++ ScriptTemplate.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sandbox.Common;
+using Sandbox.Common.Components;
+using Sandbox.Common.ObjectBuilders;
+using Sandbox.Definitions;
+using Sandbox.Engine;
+using Sandbox.ModAPI.Ingame;
+using Sandbox.ModAPI.Interfaces;
+using Sandbox.Game;
+
+namespace FirstSEMod
+{
+    public class ScriptTemplate
+    {
+        IMyGridTerminalSystem GridTerminalSystem;
+    }
+}
